445feb764ffd7f069057110d259852aed8ad1467
Ronny Bergmann <git@ronnybergmann.net>
2025-02-03 16:00:03 +0100

    Fix docs, update Readme (#8)
    * Fix documents.
    
    * Fix references.
    
    * remove windows ci.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # StatisticalManifolds
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://Owner.github.io/StatisticalManifolds.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://Owner.github.io/StatisticalManifolds.jl/dev/)
-[![Build Status](https://github.com/Owner/StatisticalManifolds.jl/actions/workflows/CI.yml/badge.svg?branch=master)](https://github.com/Owner/StatisticalManifolds.jl/actions/workflows/CI.yml?query=branch%3Amaster)
-[![Coverage](https://codecov.io/gh/Owner/StatisticalManifolds.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/Owner/StatisticalManifolds.jl)
+[![codecov](https://codecov.io/gh/JuliaManifolds/StatisticalManifolds.jl/graph/badge.svg?token=C9fJAPAx7E)](https://codecov.io/gh/JuliaManifolds/StatisticalManifolds.jl)
--- docs/make.jl
+++ docs/make.jl
@@ -13,15 +13,28 @@ if Base.active_project() != joinpath(@__DIR__, "Project.toml")
 end
 
 using StatisticalManifolds
-using Documenter
+using Documenter, DocumenterCitations, DocumenterInterLinks
 
+bib = CitationBibliography(joinpath(@__DIR__, "src", "references.bib"); style=:alpha)
+links = InterLinks(
+    # Distributions seems to still render on a too old Documenter version
+    # "Distributions.jl" => ("https://juliastats.github.io/Distributions.jl/stable/"),
+    # Integrals seems to still render on a too old Documenter version
+    # "Integrals.jl" => ("https://docs.sciml.ai/Integrals/stable/"),
+    "ManifoldsBase" => ("https://juliamanifolds.github.io/ManifoldsBase.jl/stable/"),
+    "Manifolds" => ("https://juliamanifolds.github.io/Manifolds.jl/stable/"),
+    "Manopt" => ("https://manoptjl.org/stable/"),
+    # MCIntegration seems to still render on a too old Documenter version
+    #c"MCIntegration.jl" => ("https://numericaleft.github.io/MCIntegration.jl/stable/"),
+)
 makedocs(;
     modules=[StatisticalManifolds],
     authors="Jacob T. Crosser",
     #repo="https://github.com/jcrosser/StatisticalManifolds.jl/blob/{commit}{path}#{line}",
     sitename="StatisticalManifolds.jl",
     format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true", assets=String[]),
-    pages=["Home" => "index.md"],
+    pages=["Home" => "index.md", "Literature" => "references.md"],
+    plugins=[bib, links],
 )
 
 deploydocs(;
